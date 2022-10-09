cf9ff1f358efdb2a17b70dc185f530fabc3c6dd1
Rick Owens <rick@owensmurray.com>
2022-10-09 18:28:35 -0500

    build attempt - Sun Oct  9 18:28:35 CDT 2022

--- examples/server.hs
+++ examples/server.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 {-# OPTIONS_GHC -Wwarn #-}
