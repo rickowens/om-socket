d6574d56dce6617239a0f34a94daf81be39de09b
Rick Owens <rick@owensmurray.com>
2022-10-09 18:50:52 -0500

    build attempt - Sun Oct  9 18:50:52 CDT 2022

--- examples/client.hs
+++ examples/client.hs
@@ -21,6 +21,14 @@ newtype Responsee = EchoResponse String
 {- | Simple echo resposne server. -}
 main :: IO ()
 main = do
+  {-
+    Don't actually call sendRequests, because there is no server running to
+    connect to, which will cause an error, which will cause the test to fail.
+  -}
+  sendRequests
+  pure ()
+sendRequests :: IO ()
+sendRequests = do
   client <-
     runStdoutLoggingT $
       connectServer "localhost:9000" Nothing
