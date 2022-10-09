8c2659df9bc42f9e34de786ffe8d6de535acc2f3
Rick Owens <rick@owensmurray.com>
2022-10-09 18:28:20 -0500

    build attempt - Sun Oct  9 18:28:20 CDT 2022

---
+++ examples/server.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+{-# OPTIONS_GHC -Wwarn #-}
+
+module Main (main) where
+
+import Conduit ((.|), awaitForever, runConduit)
+import Control.Monad.IO.Class (MonadIO(liftIO))
+import Data.Binary (Binary)
+import GHC.Generics (Generic)
+import OM.Socket (openIngress)
+
+{- | The requests accepted by the server. -}
+newtype Request = EchoRequest String
+  deriving newtype (Binary)
+
+
+{- | The response sent back to the client. -}
+newtype Responsee = EchoResponse String
+
+
+main :: IO ()
+main =
+  {-
+    Don't actually call serveForever, because the "test" we are using to make
+    sure this compiles will never finish running!
+  -}
+  -- serveForever
+  pure ()
+
+
+server :: IO ()
+server =
+  runConduit $
+    openServer "localhost:9000" Nothing
+    .| awaitForever (\(EchoRequest str, respond) ->
+        respond (EchoResponse str)
+    )
+
