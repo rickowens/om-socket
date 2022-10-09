8ae58ff3c612b9d15428712bf3ac0b37d1bf99d8
Rick Owens <rick@owensmurray.com>
2022-10-09 16:49:41 -0500

    build attempt - Sun Oct  9 16:49:41 CDT 2022

---
+++ examples/ingress.hs
@@ -0,0 +1,25 @@
+
+module Main (main) where
+
+import Data.Binary
+import OM.Socket
+import Conduit
+
+{- | The messages that arrive on the socket. -}
+data Msg
+  = A
+  | B
+  deriving stock (Generic)
+  deriving anyclass (Binary)
+
+main :: IO ()
+main = do
+  runConduit $
+    openIngress "localhost:9000"
+    .| awaitForever (\msg ->
+         case msg of
+           A -> putStrLn "Got A"
+           B -> putStrLn "Got B"
+       )
+
+  
