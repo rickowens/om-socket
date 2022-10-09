2e75f3beac423fb02a014a70c8b8eeaac2c316d0
Rick Owens <rick@owensmurray.com>
2022-10-09 18:45:10 -0500

    build attempt - Sun Oct  9 18:45:10 CDT 2022

---
+++ examples/client.hs
@@ -0,0 +1,26 @@
+module Main (main) where
+
+import Conduit ((.|), awaitForever, runConduit)
+import Control.Monad.Logger (runStdoutLoggingT)
+import Control.Monad.Trans.Class (MonadTrans(lift))
+import Data.Binary (Binary)
+import OM.Socket
+
+{- | The requests accepted by the server. -}
+newtype Request = EchoRequest String
+  deriving newtype (Binary, Show)
+
+
+{- | The response sent back to the client. -}
+newtype Responsee = EchoResponse String
+  deriving newtype (Binary, Show)
+
+
+{- | Simple echo resposne server. -}
+main :: IO ()
+main = do
+  client <- connectClient "localhost:9000"
+  putStrLn =<< client (EchoRequest "hello")
+  putStrLn =<< client (EchoRequest "world")
+
+
